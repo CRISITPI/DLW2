720829e55a4086793a0aafcae155d76112479ae1
Abhinav Singh <abhinav7693ngh@gmail.com>
2019-09-25 14:56:30 +0530

    Update settings.py

--- dlw_integrate/settings.py
+++ dlw_integrate/settings.py
@@ -76,15 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dlw_integrate.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
