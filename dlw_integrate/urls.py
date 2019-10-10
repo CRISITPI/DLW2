d209a50d555b1a8ac446e3168ef555a2047b26e8
AJSWAROOP <2016002@iiitdmj.ac.in>
2019-10-10 15:28:21 +0530

    Changes

--- dlw_integrate/urls.py
+++ dlw_integrate/urls.py
@@ -15,17 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from dlw.views import m4getwono,m4getdoc_no,m4getpart_no,m4getassly,m4getbr,m4view,m2getwono,m2getdoc_no,m2getpart_no,m2getassly,m2getbr,m2view,insert_machining_of_air_box,shiftsave,test,getshopempinfo,update_emp_shift_admin,update_emp_shift,update_permission_incharge,update_permission,getPermissionInfo,login_request,logout_request,homeadmin,create,homeuser,ChartData,dynamicnavbar,getEmpInfo,delete_user,forget_password,forget_path,getauthempInfo,checkloco,jpo,getYrDgp,bprodplan
-=======
-<<<<<<< HEAD
 from dlw.views import m14getwono,m14getdoc_no,m14getpart_no,m14getassly,m14getbr,m14view,m4getwono,m4getdoc_no,m4getpart_no,m4getassly,m4getbr,m4view,m2getwono,m2getdoc_no,m2getpart_no,m2getassly,m2getbr,m2view,insert_machining_of_air_box,shiftsave,test,getshopempinfo,update_emp_shift_admin,update_emp_shift,update_permission_incharge,update_permission,getPermissionInfo,login_request,logout_request,homeadmin,create,homeuser,ChartData,dynamicnavbar,getEmpInfo,delete_user,forget_password,forget_path,getauthempInfo,checkloco,jpo,getYrDgp,bprodplan
->>>>>>> master
 from dlw.views import dpo,dpoinput,checktotal,m1getpano,m1view,m5getwono,m5getdoc_no,m5getpart_no,m5getbr,m5view,airbox_addbo
-=======
-from dlw.views import m4getwono,m4getdoc_no,m4getpart_no,m4getassly,m4getbr,m4view,m2getwono,m2getdoc_no,m2getpart_no,m2getassly,m2getbr,m2view,insert_machining_of_air_box,shiftsave,test,getshopempinfo,update_emp_shift_admin,update_emp_shift,update_permission_incharge,update_permission,getPermissionInfo,login_request,logout_request,homeadmin,create,homeuser,ChartData,dynamicnavbar,getEmpInfo,delete_user,forget_password,forget_path,getauthempInfo,checkloco,jpo,getYrDgp,bprodplan
-from dlw.views import dpo,dpoinput,checktotal,m1getpano,m1view,m5getwono,m5getdoc_no,m5getpart_no,m5getbr,m5view,airbox_addbo,m5getstaff_no
->>>>>>> ca8823a72fde0238cda57a9a022e92c143c43cf1
 from dlw.views import m1genrept1,miscellaneous_section,miscell_addbo,axle_addbo,axlewheelmachining_section,m3sub,m3getdoc_no,m3getpart_no,m3getassly,m3shopsec,m3getbr,m3view,m3getwono
 from django.contrib.auth import views as auth_views
 
@@ -70,6 +61,12 @@ urlpatterns = [
     path('ajax/m4getpart_no/',m4getpart_no,name='m4getpart_no'),
     path('ajax/m4getdoc_no/',m4getdoc_no,name='m4getdoc_no'),
     path('ajax/m4getwono',m4getwono,name='m4getwono'),
+    path('m14view/', m14view, name='m14view'),
+    path('ajax/m14getbr/',m14getbr,name='m14getbr'),
+    path('ajax/m14getassly/',m14getassly,name='m14getassly'),
+    path('ajax/m14getpart_no/',m14getpart_no,name='m14getpart_no'),
+    path('ajax/m14getdoc_no/',m14getdoc_no,name='m14getdoc_no'),
+    path('ajax/m14getwono',m14getwono,name='m14getwono'),
     path('ajax/get_yr_dgp/',getYrDgp,name='get_yr_dgp'),
     path('ajax/check_loco/',checkloco,name='check_loco'),
     path('ajax/check_total/',checktotal,name='check_total'),
@@ -88,7 +85,6 @@ urlpatterns = [
     path('ajax/m5getpart_no/',m5getpart_no,name='m5getpart_no'),
     path('ajax/m5getdoc_no/',m5getdoc_no,name='m5getdoc_no'),
     path('ajax/m5getwono',m5getwono,name='m5getwono'),
-    path('ajax/m5getstaff_no/',m5getstaff_no,name='m5getstaff_no'),
     path('ajax/airbox_addbo',airbox_addbo,name='airbox_addbo'),
     path('ajax/miscell_addbo',miscell_addbo,name='miscell_addbo'),
     path('ajax/axle_addbo',axle_addbo,name='axle_addbo'),
