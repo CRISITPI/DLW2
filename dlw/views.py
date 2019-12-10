a9a111435553ad1ae8f8455c5c8d66884db1d138
abhishekshukla86 <cris.abhishekshukla@gmail.com>
2019-12-10 18:57:36 +0530

    partallotemnt views urls

--- dlw/views.py
+++ dlw/views.py
@@ -14065,10 +14065,6 @@ def mg9compreportviews(request):
         
     
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
     return render(request,"mg9compreportviews.html",context)
 
 def mg9getmwno(request):
@@ -16109,8 +16105,59 @@ def mg9getstaffno(request):
 @login_required
 @role_required(urlpass='/partallotement/')
 def partallotement(request):
-    
-    return render(request, 'partallotement.html')
+    cuser=request.user
+    usermaster=empmast.objects.filter(empno=cuser).first()
+    rolelist=usermaster.role.split(", ")
+    nav=dynamicnavbar(request,rolelist)
+    menulist=set()
+    for ob in nav:
+        menulist.add(ob.navitem)
+    menulist=list(menulist)
+    subnav=subnavbar.objects.filter(parentmenu__in=menulist) 
+    partnew = list(Partnew.objects.all().values('part_no').distinct())
+    partgrp = list(Partgrp.objects.all().values('maj_grp').distinct())
+
+    if "Superuser" in rolelist:
+        tm=shop_section.objects.all()
+        tmp=[]
+        for on in tm:
+            tmp.append(on.section_code)
+        context={
+            'sub':0,
+            'lenm' :2,
+            'nav':nav,
+            'ip':get_client_ip(request),
+            'roles':tmp,
+            'subnav':subnav,
+            'partnew' : partnew,
+            'partgrp' : partgrp,        
+        }
+    return render(request, 'partallotement.html',context)
+
+
+def getpartnewdetails(request):
+    if request.method == "GET" and request.is_ajax():        
+        partno_temp = request.GET.get("partno_temp")
+        
+        partnew = list(Partnew.objects.filter(part_no=partno_temp).values('gm_ptno','rev','des','mb','unit','size_pc','mat_specn','ind_buy','it_cat','unit_wt').distinct())
+
+        print('partnew : ----',partnew)
+        return JsonResponse(partnew, safe = False)
+    return JsonResponse({"success":False}, status=400)
+
+def getpartnewdetails123(request):
+    if request.method == "GET" and request.is_ajax():        
+        partgrp_temp = request.GET.get("maj_grp_temp")        
+        partgrp = list(Partgrp.objects.filter(maj_grp = partgrp_temp).values('sub_grp'))
+        return JsonResponse(partgrp, safe = False)
+    return JsonResponse({"success":False}, status=400)
+
+def getpartdecription(request):
+    if request.method == "GET" and request.is_ajax():        
+        subgrp_temp_temp = request.GET.get("subgrp_temp")        
+        partgrp = list(Partgrp.objects.filter(sub_grp = subgrp_temp_temp).values('sub_descr'))
+        return JsonResponse(partgrp, safe = False)
+    return JsonResponse({"success":False}, status=400)
 
 
 
